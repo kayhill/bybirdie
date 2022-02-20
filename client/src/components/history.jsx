65163f8b8d39a2de7dc3290d46f91796de5f8299
Kayliegh.Hill <kayliegh.hill@gmail.com>
2022-02-20 17:18:07 -0500

    updated useEffect in history componenet

--- client/src/components/history.jsx
+++ client/src/components/history.jsx
@@ -15,7 +15,6 @@ const History = ({ user }) => {
           setItems(items);
         },
         (error) => {
-          setIsLoaded(true);
           setError('Sorry, we encountered a problem getting your history.');
           console.log(error);
         }
@@ -24,15 +23,15 @@ const History = ({ user }) => {
 
   return (
     <div>
-      {!isLoaded && <p>Fetching your history...</p>}
+      
       {error && <p>{error}</p>}
-      {isLoaded && items.length === 0 (
+      {!isLoaded && (
         <p>
           You haven't saved any bird watching sessions yet.{' '}
           <Link to="/session">Begin birding!</Link>
         </p>
       )}
-      {isLoaded && items.length > 0 && (
+      {isLoaded && (
         <ul>
           {items.map((item, i) => {
             return (
